bba35c9df74715afab27f236b9542f06993ec68d
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-04 20:44:55 +0200

    Instalamos un nuevo componente sacado de angular material para tener un menu de barra lateral ya responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { ContactComponent } from './components/contact/contact.component';
 import { ViewProjectComponent } from './components/view-project/view-project.component';
 import { ListProjectsComponent } from './components/list-projects/list-projects.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -15,12 +22,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeComponent,
     ContactComponent,
     ViewProjectComponent,
-    ListProjectsComponent
+    ListProjectsComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
