61a598f239a1e5f749e16d84ddead4f7598d7b09
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-11 23:38:47 +0200

    Creamos un primer elemento dentro del array de Proyectos

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Project } from '../../models/project';
+import { Project } from '../models/project';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
+  arrProyectos: Project[]; /* Este array de proyectos lo consultaremos con lo que nos traigamos del back */
+  constructor() { 
 
-  constructor() { }
+    this.arrProyectos = new Array(
+      {
+        nombre: 'JavierLuqueBernier',
+        descripcion: 'Esto es un curriculum',
+        imagenes: [],
+        url: 'https://twitter.com/Sr_kelevra',
+        ano: 2015,
+        cliente: 'Yo mismo',
+        urlClient: 'https://twitter.com/Sr_kelevra',
+        categoria: 'web',
+        tecnologias: 'de todo un poco',
+      }
+    );
+
+
+
+  }
 }
