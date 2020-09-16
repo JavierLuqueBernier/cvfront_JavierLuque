12d90ee8606607d3ac79dd74475bb6bcabdab6ab
DESKTOP-35TADGG\alaga <alagaesia2004@hotmail.com>
2020-09-16 17:54:21 +0200

    Creamos una funcion para obtener el token siempre con getToken(). En principio no va a devolver nada, porque lo que vamos a hacer es almacenar el token en localStorage. De ahí que lleve un VOID. Creamos una variable objetoToken porque nos va a llegar un objeto. Llamamos al método Get dentro de httpClient a la ruta api/token. Esto me devuelve un observable, asique nos subscribimos a dicho observable para estar pendientes del valor que nos devuelve cada vez que hay un cambio en el valor de dicho observable. Subscribe recibe un callback con el valor que recibo, por eso lo llamo valor, y lo vamos a almacenar dentro de objetoToken.

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { observable } from 'rxjs';
 import { Project } from '../models/project';
 
 @Injectable({
@@ -10,6 +11,15 @@ export class ProjectsService {
   baseUrl: string;
   constructor(private httpClient: HttpClient) { /* HttpClient nos servira para poder hacer las peticiones */
     this.baseUrl = 'https://cvback-javierluque.herokuapp.com/api/proyectos';
+  }
+
+  getToken(): void {
+    let objetoToken: any;
+    this.httpClient.get('https://cvback-javierluque.herokuapp.com/api/token').subscribe( valor => {
+      objetoToken = valor;
+      console.log(objetoToken);
+    });
+    /* esto es un observable, asique hay que subscribirse */
   }
 
   getProjects() {
